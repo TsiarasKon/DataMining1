e25925f7077d09126d6fcf45ca9be80c955b480a
TsiarasKon <tsiaraskon@gmail.com>
2018-04-21 11:14:48 +0300

    Wordcloud with image mask

--- wcloud.py
+++ wcloud.py
@@ -1,6 +1,8 @@
 from wordcloud import WordCloud
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 import pandas as pd
 
 custom_stopwords = set(ENGLISH_STOP_WORDS)
@@ -11,6 +13,7 @@ train_data = pd.read_csv('train_set.csv', sep="\t")
 
 Categories = list(set(train_data['Category']))
 category_text_list = [[] for x in Categories]
+img_masks = {"Business":"business.png", "Football":"football.png", "Politics":"politics.png", "Film":"film.png", "Technology":"technology.png"}
 
 for x in zip(train_data['Content'],train_data['Category']):
 	index = Categories.index(x[1])
@@ -18,7 +21,8 @@ for x in zip(train_data['Content'],train_data['Category']):
 
 for i in range(0, len(Categories)):
 	category_text = ' '.join(category_text_list[i])
-	wc = WordCloud(width=1280, height=720, stopwords=custom_stopwords, background_color="white").generate(category_text)
+	mask = np.array(Image.open(img_masks[Categories[i]]))
+	wc = WordCloud(width=1280, height=720, stopwords=custom_stopwords, mask=mask, background_color="white").generate(category_text)
 	fig = plt.figure(figsize=(13,8))
 	plt.imshow(wc, interpolation="bilinear")
 	plt.axis("off")	
